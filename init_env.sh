ffb21506e7d33c5bdb22379a9909f9b088ca4e64
Ismael Angelo A. Casimpan Jr <ismael.angelo@casimpan.com>
2016-08-20 07:02:55 +0000

    init_env.sh: added SHCF_PLATFORM_ROOT/bin to PATH to make the tool usabe anywhere

--- init_env.sh
+++ init_env.sh
@@ -11,9 +11,13 @@
 ##
 ## See details in https://wiki.jenkins-ci.org/display/JENKINS/Environment+Script+Plugin
 
+## TODO: Add some form of help (--help) for guidance
+##
 ## allow custom path for development purposes.
 [[ ! -z $1 ]] && export SHCF_PLATFORM_ROOT=$1 || export SHCF_PLATFORM_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/ && pwd )"
+
+## add to $PATH SHCF_PLATFORM_ROOT/bin for easy usage
+export PATH=$SHCF_PLATFORM_ROOT/bin:$PATH
 echo "Platform environment has been set. See below:"
 echo "  SHCF_PLATFORM_ROOT=$SHCF_PLATFORM_ROOT"
 bash
-
